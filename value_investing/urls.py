b8d1358290b65b02992ccbafd1b43fcd49ab2b09
David Mitchell <dave@dmitchell.dev>
2021-01-31 07:03:26 +0000

    start to add auth urls and pages

--- value_investing/urls.py
+++ value_investing/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admmin
     path('admin/', admin.site.urls),
+    # User Management
+    path('accounts/', include('django.contrib.auth.urls')),
+    # Local Apps
     path('', include('pages.urls')),
 ]
