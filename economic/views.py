9eb706a6180d4016c41829c443e6b164fb80130a
David Mitchell <dave@dmitchell.dev>
2021-05-16 22:02:57 +0100

    share price queryset and dataframe

--- economic/views.py
+++ economic/views.py
@@ -4,6 +4,10 @@ from django.http import JsonResponse
 
 from django.views import View
 
+from share_prices.models import SharePrices
+from calculated_stats.models import CalculatedStats
+import pandas as pd
+
 
 class CapeChartView(TemplateView):
 
@@ -17,6 +21,32 @@ class CapeChartDataView(View):
         x_data = [5, 6, 7, 8]
 
         param_name = "Test CAPE"
+
+        df = pd.DataFrame(CalculatedStats.objects.filter(parameter_id=225).values())
+        print(df)
+
+        # df = pd.DataFrame(SharePrices.objects.values())
+        # print(df)
+
+        # df['time_stamp'] = pd.to_datetime(df['time_stamp'], format='%Y-%m-%d')
+        # print(df)
+
+        # nov_mask = df['time_stamp'].map(lambda x: x.day) == 1
+        # df = df[nov_mask]
+
+        # print(df)
+
+        df_pivot = df.pivot(
+            columns="time_stamp",
+            index="company_id",
+            values="value",
+        )
+        
+        df_pivot = df_pivot.astype(float)
+
+        print(df_pivot)
+
+        print(df_pivot.sum())
 
         # for index, row in df.iterrows():
         #     y_data.append(row["value"])
