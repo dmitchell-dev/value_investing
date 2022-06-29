a48a55bd4cd7c12c9b8dd8d71ba1baaf77a36657
David Mitchell <dave@dmitchell.dev>
2022-06-29 22:02:50 +0100

    Further code tidy, remove unused functions

--- share_prices/management/commands/share_split_calcs.py
+++ share_prices/management/commands/share_split_calcs.py
@@ -86,37 +86,3 @@ class Command(BaseCommand):
             total_rows_added = total_rows_added + num_rows
 
         print(f"{total_rows_added} saved to database")
-
-    def _format_dataframe(self, df, company_id):
-        df.insert(0, "company_id", [company_id] * df.shape[0])
-        df = df.drop(
-            ["1. open", "2. high", "3. low", "7. dividend amount"],
-            axis=1
-            )
-        df["time_stamp"] = df.index
-        df.reset_index(drop=True, inplace=True)
-        df.rename(
-            columns={
-                "4. close": "value",
-                "5. adjusted close": "value_adjusted",
-                "6. volume": "volume",
-
-            },
-            inplace=True,
-        )
-
-        return df
-
-    def _datetime_format(self, df):
-        date_fmts = ("%Y-%m-%d", "%d/%m/%y", "%d/%m/%Y")
-        for fmt in date_fmts:
-            try:
-                df["time_stamp"] = pd.to_datetime(df["time_stamp"], format=fmt)
-                break
-            except ValueError:
-                pass
-
-        # Order datetimes
-        df.sort_values(by="time_stamp", inplace=True)
-
-        return df
