32c1131e093459f55d76a8f1eb9817d984fbbdab
David Mitchell <dave@dmitchell.dev>
2022-07-07 16:27:02 +0100

    Add management command

---
+++ portfolio/management/commands/generate_portfolio.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Generates portfolio stats"
+
+    def handle(self, *args, **kwargs):
+        print('generate_portfolio')
