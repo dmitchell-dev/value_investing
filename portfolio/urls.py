73a626b0646a039f73ef8e727042fbabc508a773
David Mitchell <dave@dmitchell.dev>
2022-07-10 10:32:50 +0100

    Try to get multiple plotly charts on a single page debug, looking like this is not possible

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,7 +7,8 @@ from .views import (
     InvestmentUpdateView,
     InvestmentDeleteView,
     PortfolioOverviewView,
-    portfolio_overview_view,
+    portfolio_overview_pie_view,
+    portfolio_overview_table_view,
     )
 
 app_name = "portfolio"
@@ -19,5 +20,6 @@ urlpatterns = [
     path("investments/<int:pk>/update/", InvestmentUpdateView.as_view(), name="investment_update"),
     path("investments/", InvestmentListView.as_view(), name="investment_list"),
     path("portfolio/", PortfolioOverviewView.as_view(), name="portfolio_overview"),
-    path("portfolio/overview-chart/", portfolio_overview_view, name="portfolio_overview_2"),
+    path("portfolio/overview-chart/", portfolio_overview_pie_view, name="portfolio_overview_pie"),
+    path("portfolio/overview-table/", portfolio_overview_table_view, name="portfolio_overview_table"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import render
 from .models import Investments, WishList, Portfolio
 
 import plotly.express as px
+from plotly.offline import plot
+import plotly.graph_objects as go
 import pandas as pd
 
 
@@ -87,7 +89,7 @@ class PortfolioOverviewView(TemplateView):
         return test_df
 
 
-def portfolio_overview_view(request):
+def portfolio_overview_pie_view(request):
 
     error_message = None
 
@@ -102,19 +104,50 @@ def portfolio_overview_view(request):
         values="price",
         names="company__company_name",
         title="Test Chart",
-    )
+        )
 
-    # Getting HTML needed to render the plot.
-    plot_div = chart_plot.to_html()
+    # plot_div = plot(
+    #     {'data': chart_plot},
+    #     output_type='div'
+    #     )
+
+    plot_div = chart_plot.to_html(full_html=False, include_plotlyjs='cdn')
 
     context = {
         "graph": plot_div,
         "error_message": error_message,
-    }
+        }
 
     return render(request, 'portfolio/partials/pie-chart.html', context)
 
-    # if request.htmx:
-    #     return render(request, 'portfolio/partials/pie-chart.html', context)
-    # else:
-    #     return render(request, 'portfolio/overview.html', context)
+
+def portfolio_overview_table_view(request):
+
+    error_message = None
+
+    df = pd.DataFrame(
+            list(Investments.objects.get_table_joined())
+            )
+
+    df["price"] = df["price"].astype(float)
+
+    chart_plot2 = px.pie(
+        df,
+        values="price",
+        names="company__company_name",
+        title="Test Chart",
+        )
+
+    # plot_div = plot(
+    #     {'data': chart_plot},
+    #     output_type='div'
+    #     )
+
+    plot_div2 = chart_plot2.to_html(full_html=False, include_plotlyjs='cdn')
+
+    context = {
+        "graph2": plot_div2,
+        "error_message2": error_message,
+        }
+
+    return render(request, 'portfolio/partials/pie-chart-2.html', context)
