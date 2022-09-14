67a21da3d3484262f743eba63a3c4eef759d92ca
David Mitchell <dave@dmitchell.dev>
2022-09-14 21:55:50 +0100

    Include last dates in dashboard list

--- dashboard_company/tables.py
+++ dashboard_company/tables.py
@@ -1,5 +1,9 @@
 import django_tables2 as tables
 
+from datetime import datetime
+import pytz
+from dateutil import relativedelta
+
 from django_tables2.utils import A
 
 from .models import DashboardCompany
@@ -26,6 +30,14 @@ class DashboardCompanyTable(tables.Table):
     def render_dcf_intrinsic_value(self, value, record):
         return f"{record.currency_symbol}{value:.2f}"
 
+    def render_latest_financial_date(self, value, record):
+        delta = relativedelta.relativedelta(datetime.now(pytz.utc), record.latest_financial_date)
+        return f"{delta.years} Years, {delta.months} Months, {delta.days} Days"
+
+    def render_latest_share_price_date(self, value, record):
+        delta = relativedelta.relativedelta(datetime.now(pytz.utc), record.latest_share_price_date)
+        return f"{delta.years} Years, {delta.months} Months, {delta.days} Days"
+
     class Meta:
         model = DashboardCompany
         attrs = {"class": "table thead-light table-striped table-hover"}
@@ -37,4 +49,6 @@ class DashboardCompanyTable(tables.Table):
             "dcf_intrinsic_value",
             "margin_safety",
             "latest_margin_of_safety",
+            "latest_financial_date",
+            "latest_share_price_date",
         )
